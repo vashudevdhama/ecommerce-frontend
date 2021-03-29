9961ec2d07794d9543a300a0f6eaa6eb83a221d0
vashudevdhama <dhamavashu1119@gmail.com>
2021-03-29 22:10:44 +0530

    feat: Main content flex layout

--- src/components/PageLayouts/MainContent.js
+++ src/components/PageLayouts/MainContent.js
@@ -1,9 +1,90 @@
 import React from 'react';
+import prod1 from '../../assets/prod1.jpg';
+import prod4 from '../../assets/prod4.jpg';
+import "./MainContent.css";
 
 function MainContent(){
     return (
-        <div>
-            Main Content
+        <div className="maincontent-container">
+
+            <div className="maincontent-container-item">
+                <div className="maincontent-container-item__image">
+                    <img src={prod1} alt="product one" />
+                </div>
+                <div className="maincontent-container-item__info">
+                    <div className="maincontent-container-item__info-title">Title</div>
+                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
+                </div>
+            </div>
+
+            <div className="maincontent-container-item">
+                <div className="maincontent-container-item__image">
+                    <img src={prod4} alt="product one" />
+                </div>
+                <div className="maincontent-container-item__info">
+                    <div className="maincontent-container-item__info-title">Title</div>
+                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
+                </div>
+            </div>
+
+            <div className="maincontent-container-item">
+                <div className="maincontent-container-item__image">
+                    <img src={prod4} alt="product one" />
+                </div>
+                <div className="maincontent-container-item__info">
+                    <div className="maincontent-container-item__info-title">Title</div>
+                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
+                </div>
+            </div>
+            <div className="maincontent-container-item">
+                <div className="maincontent-container-item__image">
+                    <img src={prod1} alt="product one" />
+                </div>
+                <div className="maincontent-container-item__info">
+                    <div className="maincontent-container-item__info-title">Title</div>
+                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
+                </div>
+            </div>
+
+            <div className="maincontent-container-item">
+                <div className="maincontent-container-item__image">
+                    <img src={prod1} alt="product one" />
+                </div>
+                <div className="maincontent-container-item__info">
+                    <div className="maincontent-container-item__info-title">Title</div>
+                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
+                </div>
+            </div>
+
+            <div className="maincontent-container-item">
+                <div className="maincontent-container-item__image">
+                    <img src={prod4} alt="product one" />
+                </div>
+                <div className="maincontent-container-item__info">
+                    <div className="maincontent-container-item__info-title">Title</div>
+                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
+                </div>
+            </div>
+
+            <div className="maincontent-container-item">
+                <div className="maincontent-container-item__image">
+                    <img src={prod4} alt="product one" />
+                </div>
+                <div className="maincontent-container-item__info">
+                    <div className="maincontent-container-item__info-title">Title</div>
+                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
+                </div>
+            </div>
+            <div className="maincontent-container-item">
+                <div className="maincontent-container-item__image">
+                    <img src={prod1} alt="product one" />
+                </div>
+                <div className="maincontent-container-item__info">
+                    <div className="maincontent-container-item__info-title">Title</div>
+                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
+                </div>
+            </div>
+
         </div>
     )
 }
