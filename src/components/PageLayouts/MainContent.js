54c14498501b1768a285164156a25b58103cc9ca
vashudevdhama <dhamavashu1119@gmail.com>
2021-03-30 21:40:38 +0530

    feat: product img onhover overlay

--- src/components/PageLayouts/MainContent.js
+++ src/components/PageLayouts/MainContent.js
@@ -1,90 +1,18 @@
 import React from 'react';
 import prod1 from '../../assets/prod1.jpg';
 import prod4 from '../../assets/prod4.jpg';
+import ProductCard from '../ProductCard/ProductCard';
 import "./MainContent.css";
 
 function MainContent(){
     return (
         <div className="maincontent-container">
 
-            <div className="maincontent-container-item">
-                <div className="maincontent-container-item__image">
-                    <img src={prod1} alt="product one" />
-                </div>
-                <div className="maincontent-container-item__info">
-                    <div className="maincontent-container-item__info-title">Title</div>
-                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
-                </div>
-            </div>
-
-            <div className="maincontent-container-item">
-                <div className="maincontent-container-item__image">
-                    <img src={prod4} alt="product one" />
-                </div>
-                <div className="maincontent-container-item__info">
-                    <div className="maincontent-container-item__info-title">Title</div>
-                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
-                </div>
-            </div>
-
-            <div className="maincontent-container-item">
-                <div className="maincontent-container-item__image">
-                    <img src={prod4} alt="product one" />
-                </div>
-                <div className="maincontent-container-item__info">
-                    <div className="maincontent-container-item__info-title">Title</div>
-                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
-                </div>
-            </div>
-            <div className="maincontent-container-item">
-                <div className="maincontent-container-item__image">
-                    <img src={prod1} alt="product one" />
-                </div>
-                <div className="maincontent-container-item__info">
-                    <div className="maincontent-container-item__info-title">Title</div>
-                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
-                </div>
-            </div>
-
-            <div className="maincontent-container-item">
-                <div className="maincontent-container-item__image">
-                    <img src={prod1} alt="product one" />
-                </div>
-                <div className="maincontent-container-item__info">
-                    <div className="maincontent-container-item__info-title">Title</div>
-                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
-                </div>
-            </div>
-
-            <div className="maincontent-container-item">
-                <div className="maincontent-container-item__image">
-                    <img src={prod4} alt="product one" />
-                </div>
-                <div className="maincontent-container-item__info">
-                    <div className="maincontent-container-item__info-title">Title</div>
-                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
-                </div>
-            </div>
-
-            <div className="maincontent-container-item">
-                <div className="maincontent-container-item__image">
-                    <img src={prod4} alt="product one" />
-                </div>
-                <div className="maincontent-container-item__info">
-                    <div className="maincontent-container-item__info-title">Title</div>
-                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
-                </div>
-            </div>
-            <div className="maincontent-container-item">
-                <div className="maincontent-container-item__image">
-                    <img src={prod1} alt="product one" />
-                </div>
-                <div className="maincontent-container-item__info">
-                    <div className="maincontent-container-item__info-title">Title</div>
-                    <div className="maincontent-container-item__info-subinfo">Subinfo</div>
-                </div>
-            </div>
-
+            <ProductCard imgsrc={prod1} title="Title" price="Rs. 0.0" ratings="4.9/5" />
+            <ProductCard imgsrc={prod4} title="Title" price="Rs. 0.0" ratings="4.3/5" />
+            <ProductCard imgsrc={prod1} title="Title" price="Rs. 0.0" ratings="4.5/5" />
+            <ProductCard imgsrc={prod1} title="Title" price="Rs. 0.0" ratings="4.9/5" />
+        
         </div>
     )
 }
